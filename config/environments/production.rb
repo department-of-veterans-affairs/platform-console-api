3056837232ec0718e23d8dfa096b8dd5c45230f3
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2022-01-28 12:44:39 -0500

    rubocop single quotes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.hosts << "platform-console-api.vfs.va.gov"
+  config.hosts << 'platform-console-api.vfs.va.gov'
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
