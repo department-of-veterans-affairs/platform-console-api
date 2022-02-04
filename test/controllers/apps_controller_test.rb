812dc4d8ae181cd6cced298ee46acc7ca45bf336
Travis Hilton <travis.hilton@oddball.io>
2022-02-04 11:10:46 -0500

    fix: Cleaning up views and controllers for apps and teams

--- test/controllers/apps_controller_test.rb
+++ test/controllers/apps_controller_test.rb
@@ -5,8 +5,7 @@ require 'test_helper'
 class AppsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user =  users(:john)
-    token = @user.perishable_token
-    post "/login?token=#{token}"
+    post "/login?uid=#{@user.uid}"
     @team = teams(:one)
     @app = apps(:one)
   end
