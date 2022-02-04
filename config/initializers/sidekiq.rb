69a5893de2451478d137082e40d05f47a14446d4
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2022-02-04 13:54:14 -0500

    use rails_env var

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,7 +3,7 @@
 rails_root = Rails.root || File.dirname(__FILE__) + '/../..'
 rails_env = Rails.env || 'development'
 redis_config = YAML.load_file(rails_root.to_s + '/config/redis.yml')
-redis_config.merge! redis_config.fetch(Rails.env, {})
+redis_config.merge! redis_config.fetch(rails_env, {})
 redis_config.symbolize_keys!
 
 Sidekiq.configure_server do |config|
