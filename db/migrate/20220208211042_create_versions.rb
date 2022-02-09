61218da39bfbe94b69a9030d73c30d59303331a5
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2022-02-09 14:09:50 -0500

    remove mysql comments

--- db/migrate/20220208211042_create_versions.rb
+++ db/migrate/20220208211042_create_versions.rb
@@ -16,22 +16,6 @@ class CreateVersions < ActiveRecord::Migration[7.0]
       t.string   :event,     null: false
       t.string   :whodunnit
       t.text     :object, limit: TEXT_BYTES
-
-      # Known issue in MySQL: fractional second precision
-      # -------------------------------------------------
-      #
-      # MySQL timestamp columns do not support fractional seconds unless
-      # defined with "fractional seconds precision". MySQL users should manually
-      # add fractional seconds precision to this migration, specifically, to
-      # the `created_at` column.
-      # (https://dev.mysql.com/doc/refman/5.6/en/fractional-seconds.html)
-      #
-      # MySQL users should also upgrade to at least rails 4.2, which is the first
-      # version of ActiveRecord with support for fractional seconds in MySQL.
-      # (https://github.com/rails/rails/pull/14359)
-      #
-      # MySQL users should use the following line for `created_at`
-      # t.datetime :created_at, limit: 6
       t.datetime :created_at
     end
     add_index :versions, %i[item_type item_id]
