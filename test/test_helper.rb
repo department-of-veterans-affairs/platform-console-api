b0b2c9ed4d73c51c24e226874b50249ca943ffd5
jhwuz <jeremyhaowu@gmail.com>
2022-02-10 14:13:15 -0700

    Allow http connections when no cassette

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,6 +16,7 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = 'test/vcr_cassettes'
   config.hook_into :webmock
+  config.allow_http_connections_when_no_cassette = true
   config.before_record do |interaction|
     interaction.request.headers.delete('Authorization')
   end
