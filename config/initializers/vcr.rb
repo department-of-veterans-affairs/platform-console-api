4c1692ad04d378b618287d7fd55be0099f38a89d
Riley Anderson <5150r.d.anderson@gmail.com>
2022-02-11 12:14:13 -0700

    forgot to prepend

--- config/initializers/vcr.rb
+++ config/initializers/vcr.rb
@@ -3,7 +3,7 @@
 # TODO: revomve this file once this merge has been released https://github.com/vcr/vcr/pull/907/files
 
 # Overrides for fixing issues caused by ruby 3.1
-module VcrWebmocxExtensions
+module VCRWebMockExtensions
   def global_hook_disabled?(request)
     requests = Thread.current[:_vcr_webmock_disabled_requests]
     requests&.include?(request)
@@ -12,4 +12,8 @@ module VcrWebmocxExtensions
   def global_hook_disabled_requests
     Thread.current[:_vcr_webmock_disabled_requests] ||= []
   end
+end
+
+VCR::LibraryHooks::WebMock.module_eval do
+  prepend VCRWebMockExtensions
 end
