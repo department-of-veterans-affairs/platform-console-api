d3a6cde867efe0671c0fd124948eadd15c88ef1e
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2022-02-15 11:40:47 -0500

    update system test

--- test/system/audits_test.rb
+++ test/system/audits_test.rb
@@ -5,10 +5,16 @@ require 'application_system_test_case'
 class AuditsTest < ApplicationSystemTestCase
   setup do
     login_as :john
-    @team_one = teams(:one)
-    @team_two = teams(:two)
-    @app_one = apps(:one)
-    @app_two = apps(:two)
+
+    (1..3).each do |t|
+       new_app = apps(:one).dup
+       new_app.name = "App #{t}"
+       new_app.save
+
+       new_team = teams(:one).dup
+       new_team.name = "Team #{t}"
+       new_team.save
+    end
   end
 
   test 'visiting the index' do
@@ -17,9 +23,9 @@ class AuditsTest < ApplicationSystemTestCase
     assert_selector 'th', text: 'ITEM TYPE'
     assert_selector 'td', text: 'App'
     assert_selector 'td', text: 'Team'
-    assert_selector 'div', text: 'Details'
+    assert_selector 'div', text: 'DETAILS'
 
-    click_on 'Details', match: :first
+    click_on 'DETAILS', match: :first
     assert_selector 'span', text: 'Hide Details'
   end
 end
