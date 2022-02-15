17760aca8ad436f1cc6109b83c5be8ada97d7ea3
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2022-02-15 09:45:01 -0500

    update migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_08_182516) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_02_15_144301) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
