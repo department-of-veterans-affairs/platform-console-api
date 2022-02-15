b926bf10dbf2e10de4753ec7d55cc4a4f4504782
Riley Anderson <5150r.d.anderson@gmail.com>
2022-02-15 08:31:01 -0700

    workflow_run_job specs

--- lib/git_hub.rb
+++ lib/git_hub.rb
@@ -4,6 +4,7 @@ require 'git_hub/issue'
 require 'git_hub/pull_request'
 require 'git_hub/repository'
 require 'git_hub/workflow_run'
+require 'git_hub/workflow_run_job'
 require 'git_hub/workflow'
 
 # Contains classes/methods for Github API integration
