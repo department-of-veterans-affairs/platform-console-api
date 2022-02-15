7a80c50f1e8b4abea7d910e468931b8fce544cfe
Riley Anderson <5150r.d.anderson@gmail.com>
2022-02-14 19:16:00 -0700

    don't eager load

--- lib/git_hub.rb
+++ lib/git_hub.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+require 'git_hub/issue'
+require 'git_hub/pull_request'
+require 'git_hub/repository'
+require 'git_hub/workflow_run'
+require 'git_hub/workflow'
+
 # Contains classes/methods for Github API integration
 module GitHub
   GITHUB_ORGANIZATION = 'department-of-veterans-affairs'
