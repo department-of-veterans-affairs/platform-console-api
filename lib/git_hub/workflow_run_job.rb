e55f79b65f30f98c16841edc18aecf320b5f541e
Jeremy Wu <jhwuz@users.noreply.github.com>
2022-02-15 17:05:48 -0700

    add comments and rescue for check runs

--- lib/git_hub/workflow_run_job.rb
+++ lib/git_hub/workflow_run_job.rb
@@ -18,8 +18,12 @@ module GitHub
       logs = begin
         Octokit.workflow_run_job_logs("#{GITHUB_ORGANIZATION}/#{@repo}", @id)
       rescue Octokit::NotFound
-        check_run = Octokit.check_run_from_url(gh_info.check_run_url)
-        "#{check_run.output.title} \n #{check_run.output.summary}"
+        begin
+          check_run = Octokit.check_run_from_url(gh_info.check_run_url)
+          "#{check_run.output.title} \n #{check_run.output.summary}"
+        rescue Octokit::NotFound
+          ''
+        end
       end
       logs_to_html(logs)
     end
