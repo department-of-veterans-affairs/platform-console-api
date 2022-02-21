3982e58629e24c235545ea5200a5c05206d5fd2e
Riley Anderson <5150r.d.anderson@gmail.com>
2022-02-21 14:24:08 -0700

    Connect app to GitHub deploy (#124)
    * Add workflow to create PR
    
    * fix format
    
    * fix format
    
    * test
    
    * fix option name
    
    * add ref
    
    * cleanup
    
    * cleanup
    
    * dont use env
    
    * fix
    
    * deploy controller
    
    * Add deploy tab
    
    * cleanup
    
    * add wording
    
    * fix
    
    * remove unused methods
    
    * cleanup

--- lib/github.rb
+++ lib/github.rb
@@ -7,7 +7,10 @@ require 'github/repository'
 require 'github/workflow_run'
 require 'github/workflow_run_job'
 require 'github/workflow'
+
 # Contains classes/methods for Github API integration
 module Github
   GITHUB_ORGANIZATION = 'department-of-veterans-affairs'
+  CREATE_PR_WORKFLOW_FILE = 'create_deploy_pr.yml'
+  DEPLOY_WORKFLOW_FILE = 'deploy-template.yml'
 end
