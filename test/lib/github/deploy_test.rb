36543b028e4d28e43e5b7f0e5ccbf58ef20ddb84
Riley Anderson <5150r.d.anderson@gmail.com>
2022-02-23 15:35:02 -0700

    rubocop

--- test/lib/github/deploy_test.rb
+++ test/lib/github/deploy_test.rb
@@ -14,13 +14,13 @@ module Github
     test 'can be created with a valid repo and deploy id' do
       assert_instance_of Github::Deploy, @deploy
       assert_instance_of Sawyer::Resource, @deploy.github
-      assert_equal 17929736, @deploy.github.id
+      assert_equal 17_929_736, @deploy.github.id
     end
 
     test 'lists all runs for the deploy' do
       VCR.use_cassette('github/deploy', record: :new_episodes) do
         deploy_runs = @deploy.deploy_runs.workflow_runs
-        assert_equal 17929736, deploy_runs.first.workflow_id
+        assert_equal 17_929_736, deploy_runs.first.workflow_id
       end
     end
 
