e289754402d8cf3020e29151a716ecf5f9f0bfcb
Riley Anderson <5150r.d.anderson@gmail.com>
2022-02-24 09:15:29 -0700

    fix

--- app/controllers/github/pull_requests_controller.rb
+++ app/controllers/github/pull_requests_controller.rb
@@ -9,5 +9,14 @@ module Github
       @github_pull_requests = Github::PullRequest.all(@app.github_repo, @current_page)
       set_pages
     end
+
+    private
+
+    def set_pages
+      @last_page = @github_pull_requests.dig(:pages, :last)
+      @next_page = @curr_page.to_i + 1 unless @curr_page == @last_page
+      @prev_page = @curr_page.to_i - 1 unless @curr_page == 1
+      @first_page = 1
+    end
   end
 end
