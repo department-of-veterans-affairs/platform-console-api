a96865cb65d4a845b4b6c929201c8d4609558d2f
Jeremy Wu <jhwuz@users.noreply.github.com>
2022-03-03 00:07:56 -0700

    cleanup, add schema so tests pass for code cov

--- lib/github/graph_ql.rb
+++ lib/github/graph_ql.rb
@@ -5,15 +5,14 @@ require 'graphql/client/http'
 module Github
   # Module containing a graphQL client and queries for github
   module GraphQL
-    # Setup GraphQL
-    unless File.exist?(Rails.root.join('tmp/github_graphql_schema.json'))
-      # Download GraphQL schema
+    # Download GraphQL schema
+    unless File.exist?(Rails.root.join('lib/github/graph_ql/schema.json'))
       adapter = ::GraphQL::Client::HTTP.new('https://api.github.com/graphql') do
         def headers(_context)
           { 'Authorization' => "Bearer #{ENV['GITHUB_ACCESS_TOKEN']}" }
         end
       end
-      ::GraphQL::Client.dump_schema(adapter, 'tmp/github_graphql_schema.json')
+      ::GraphQL::Client.dump_schema(adapter, 'lib/github/graph_ql/schema.json')
     end
 
     # Configure GraphQL endpoint using the basic HTTP network adapter.
@@ -28,19 +27,10 @@ module Github
       end
     end
 
-    # Fetch latest schema on init, this will make a network request
-    # Schema = GraphQL::Client.load_schema(HTTP)
-
-    # However, it's smart to dump this to a JSON file and load from disk
-    #
-    # Run it from a script or rake task
-    # ::GraphQL::Client.dump_schema(HTTP, 'tmp/github_graphql_schema.json')
-    Schema = ::GraphQL::Client.load_schema('tmp/github_graphql_schema.json')
-
+    Schema = ::GraphQL::Client.load_schema('lib/github/graph_ql/schema.json')
     Client = ::GraphQL::Client.new(schema: Schema, execute: HTTP)
 
     ## Queries
-
     # Query to get various stats for the app's github repository
     GithubInfoQuery = Github::GraphQL::Client.parse <<~'GRAPHQL'
       query($owner: String!, $name: String!) {
