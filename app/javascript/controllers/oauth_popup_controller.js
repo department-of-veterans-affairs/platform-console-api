8860c93186cd93f9b940f9b2e975a8ab2408cbfc
Riley Anderson <5150r.d.anderson@gmail.com>
2022-03-03 08:06:51 -0700

    formatting

--- app/javascript/controllers/oauth_popup_controller.js
+++ app/javascript/controllers/oauth_popup_controller.js
@@ -12,7 +12,9 @@ export default class extends Controller {
    }
 
   open() {
-    window.open(this.oauth_url, 'popUpWindow','height=700,width=500,left=200,top=400,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes')
+    window.open(this.oauth_url,
+                'popUpWindow',
+                'height=700,width=500,left=200,top=300,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes')
   }
 
   close() {
