501827420f8f114d4c728b0ee78ead29f8f25d9c
Jeremy Wu <jhwuz@users.noreply.github.com>
2022-03-09 16:32:07 -0700

    still need to figure out why tests aren't passing

--- test/controllers/github/deploy_pull_requests_controller_test.rb
+++ test/controllers/github/deploy_pull_requests_controller_test.rb
@@ -5,7 +5,7 @@ require 'test_helper'
 module Github
   class DeployPullRequestsControllerTest < ActionDispatch::IntegrationTest
     setup do
-      @user = users(:john)
+      @user = users(:jane)
       setup_omniauth_mock(@user)
       @team = teams(:three)
       @app = apps(:four)
@@ -13,6 +13,9 @@ module Github
 
     test 'should create a deploy pull request' do
       VCR.use_cassette('github/deploy_pull_requests_controller') do
+        # TODO: need to fix auth
+        get github_oauth_callback_path(code: '1b5acd1175129a747f29')
+        @user.reload
         post team_app_deploy_pull_requests_path(@team, @app)
         assert_redirected_to team_app_deploys_path
       end
--- test/system/github/workflow_run_jobs_test.rb
+++ test/system/github/workflow_run_jobs_test.rb
@@ -7,17 +7,22 @@ module Github
     setup do
       login_as :john
       @team = teams(:three)
-      @app = apps(:three)
+      @app = apps(:four)
     end
 
     test 'visiting the index' do
       VCR.use_cassette('system/github/workflow_run_jobs') do
-        visit team_app_workflow_run_job_path(@team, @app, 5_204_164_825, workflow_run_id: 1_848_333_333)
+        visit team_app_workflow_run_job_path(@team, @app, 5_487_333_352, workflow_run_id: 1_959_838_157)
         assert_selector 'a', text: 'Workflows'
         assert_selector 'a', text: 'All Jobs'
         assert_selector 'p', text: 'Logs'
         assert_selector(:link, nil, title: 'Download Logs')
         assert_selector(:link, nil, href: %r{workflow_run_jobs/\d+$})
+        assert_selector(:button, nil, title: 'Restart Run')
+
+        # TODO: No route matches, missing required keys: [:id] in workflow_runs_controller.rb:47
+        click_on 'Restart Run', match: :first
+        assert_selector 'p', text: 'Workflow run was sucessfully restarted'
       end
     end
   end
