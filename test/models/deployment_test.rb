0c16c7ea1a8ad187a64c31c31f3e556a09426f0d
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2022-03-09 12:51:00 -0500

    add some deployment tests

--- test/models/deployment_test.rb
+++ test/models/deployment_test.rb
@@ -3,7 +3,25 @@
 require 'test_helper'
 
 class DeploymentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @app = apps(:one)
+    @team = teams(:one)
+    @deployment = deployments(:one)
+  end
+
+  test 'valid team' do
+    @deployment.update(name: 'deployment-test')
+    assert @deployment.valid?
+  end
+
+  test 'valid team with papertrail versions' do
+    @deployment.update(name: 'deployment-test')
+    assert @deployment.valid?
+    assert_not_nil @deployment.versions
+  end
+
+  test 'invalid team' do
+    deployment = Deployment.new
+    assert_not deployment.valid?
+  end
 end
