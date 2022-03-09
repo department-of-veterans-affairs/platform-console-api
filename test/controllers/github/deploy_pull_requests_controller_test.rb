a905577440eee20ce8ef7daf53476653ca6b9454
Jeremy Wu <jhwuz@users.noreply.github.com>
2022-03-09 15:08:26 -0700

    add deploy pull requests controller test

---
+++ test/controllers/github/deploy_pull_requests_controller_test.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Github
+  class DeployPullRequestsControllerTest < ActionDispatch::IntegrationTest
+    setup do
+      @user = users(:john)
+      setup_omniauth_mock(@user)
+      @team = teams(:three)
+      @app = apps(:four)
+    end
+
+    test 'should create a deploy pull request' do
+      VCR.use_cassette('github/deploy_pull_requests_controller') do
+        post team_app_deploy_pull_requests_path(@team, @app)
+        assert_redirected_to team_app_deploys_path
+      end
+    end
+  end
+end
