50fa98788964a7e722aab73075da5ea83ea93e53
Jeremy Wu <jhwuz@users.noreply.github.com>
2022-03-09 22:20:42 -0700

    allow repeat playbacks on cassette

--- test/controllers/github/deploy_pull_requests_controller_test.rb
+++ test/controllers/github/deploy_pull_requests_controller_test.rb
@@ -12,7 +12,7 @@ module Github
     end
 
     test 'should create a deploy pull request' do
-      VCR.use_cassette('github/deploy_pull_requests_controller') do
+      VCR.use_cassette('github/deploy_pull_requests_controller', allow_playback_repeats: true) do
         get github_oauth_callback_path(code: '5a789efeb8e9226de745')
         @user.reload
         post team_app_deploy_pull_requests_path(@team, @app)
