9d345225326e3604fd62e98e9345192fe70e9304
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2022-03-10 14:07:29 -0500

    fix: test didn't work

--- lib/argo_cd/client.rb
+++ lib/argo_cd/client.rb
@@ -29,10 +29,6 @@ module ArgoCd
       https.verify_mode = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
       response = https.get(uri, request_headers)
 
-      if Rails.env.production?
-        response = Net::HTTP.get_response(URI.parse(response['location']))
-      end
-
       Response.new(response: response)
     end
 
@@ -67,11 +63,6 @@ module ArgoCd
       request = build_request(uri)
 
       r = https.request(request)
-
-      if Rails.env.production?
-        r = Net::HTTP.get_response(URI.parse(r['location']))
-      end
-
       response = Response.new(response: r)
 
       save_token(response) if response.successful?
