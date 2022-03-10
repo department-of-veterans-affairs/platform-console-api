96f5f352505612d837dcc874088a91a1cb379b8c
Jeremy Wu <jhwuz@users.noreply.github.com>
2022-03-10 09:42:26 -0700

    fix deploy pr test

--- test/controllers/github/deploy_pull_requests_controller_test.rb
+++ test/controllers/github/deploy_pull_requests_controller_test.rb
@@ -5,7 +5,7 @@ require 'test_helper'
 module Github
   class DeployPullRequestsControllerTest < ActionDispatch::IntegrationTest
     setup do
-      @user = users(:jane)
+      @user = users(:john)
       setup_omniauth_mock(@user)
       @team = teams(:three)
       @app = apps(:four)
@@ -13,8 +13,6 @@ module Github
 
     test 'should create a deploy pull request' do
       VCR.use_cassette('github/deploy_pull_requests_controller', allow_playback_repeats: true) do
-        get github_oauth_callback_path(code: '5a789efeb8e9226de745')
-        @user.reload
         post team_app_deploy_pull_requests_path(@team, @app)
         assert_redirected_to team_app_deploys_path
         assert_equal 'Pull Request has been created.', flash.notice
