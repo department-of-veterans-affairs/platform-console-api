f871be697e63b5cc5bc9044170625deef2b4ba39
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2022-03-14 15:48:04 -0400

    test argo api env var

--- test/system/deployments_test.rb
+++ test/system/deployments_test.rb
@@ -13,6 +13,7 @@ class DeploymentsTest < ApplicationSystemTestCase
 
     @app_two = apps(:three)
     @deployment_two = deployments(:two)
+    puts "!!!!!#{ENV['ARGO_API']}"
   end
 
   test 'visiting the index' do
