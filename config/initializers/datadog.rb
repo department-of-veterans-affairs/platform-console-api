2e5f54dbaf5f3d24f1df82e03a3aa2c2eb02051f
Riley Anderson <5150r.d.anderson@gmail.com>
2022-03-21 11:51:15 -0600

    Only enable datadog in production

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -3,7 +3,7 @@
 Datadog.configure do |c|
   c.use :rails
   c.service = 'platform-console-api'
-  c.tracer.enabled = true
+  c.tracer.enabled = Rails.env.production?
   c.tracer hostname: 'datadog-agent',
            port: 8126
 end
