522111103a473b8cd26dd3d3fddff2755c654873
rccassity <rccassity@gmail.com>
2022-03-23 13:19:52 -0500

    rm token

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -3,7 +3,7 @@
 require 'slack-ruby-client'
 
 Slack.configure do |config|
-  config.token = 'xoxp-3214246874021-3229753718593-3246033823872-f0e6fdb4ea4e8024b1d218a64c5e18ce'
+  config.token = ENV['SLACK_TOKEN']
 end
 
 
