f72fc89b5662810f781db417d1a8539f431e4aa0
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2022-04-25 15:29:00 -0400

    keycloak_access_token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_15_144355) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_08_181428) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_15_144355) do
     t.string "uid"
     t.string "github_token"
     t.string "argo_token"
+    t.string "keycloak_access_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["password_digest"], name: "index_users_on_password_digest"
     t.index ["uid"], name: "index_users_on_uid"
