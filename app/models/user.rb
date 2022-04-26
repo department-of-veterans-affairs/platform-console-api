e5c1ac50acecda1907f2948fee8919d6615d8d40
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2022-04-26 09:57:22 -0400

    save api token per login

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   # before_destroy :revoke_keycloak_token
 
   def self.from_omniauth(auth_hash)
-    User.find_or_initialize_by(uid: auth_hash['uid']) do |u|
+    user = User.find_or_initialize_by(uid: auth_hash['uid']) do |u|
       u.name = auth_hash['info']['name']
       u.email = auth_hash['info']['email']
       # Tokens have a short expiration date.
@@ -25,6 +25,8 @@ class User < ApplicationRecord
       u.save!
     end
 
+    user.save_api_token(auth_hash) # save token on existing users per login
+    user
     # Authorize user and ensure keycloak is the provider
     # teams = auth_hash['extra']['raw_info']['groups']
     # roles = auth_hash['extra']['raw_info']['resource_access']['account']['roles']
