55cbae433599f510393ba7c4bef9f118d43687fa
rccassity <rccassity@gmail.com>
2022-04-28 13:49:01 -0500

    Removed keycloak_access_token

--- db/migrate/20220408181428_add_keycloak_access_token_to_user.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Add keycloak access token to users table
-class AddKeycloakAccessTokenToUser < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :keycloak_access_token, :string
-  end
-end
