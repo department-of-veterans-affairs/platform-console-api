6dd3cb016f1243e9454de2d246a0995a9c01606c
jhwuz <jeremyhaowu@gmail.com>
2022-06-13 13:04:41 -0600

    fix deploy_pull_requests controller

--- app/controllers/api/v1/github/deploy_pull_requests_controller.rb
+++ app/controllers/api/v1/github/deploy_pull_requests_controller.rb
@@ -8,7 +8,7 @@ module Api
         # POST /v1/teams/:team_id/apps/:app_id/deploy_pull_requests
         def create
           if @app.repository(current_user.github_token).create_deploy_workflow_pr
-            render json: true, status: ok
+            render json: true, status: :ok
           else
             render json: false, status: :unprocessable_entity
           end
