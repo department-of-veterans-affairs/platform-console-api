054685d8cd28e30d8b156544c98b06ac3f637614
jhwuz <jeremyhaowu@gmail.com>
2022-06-29 12:06:31 -0600

    add repository docs

--- spec/requests/api/v1/github/repositories_spec.rb
+++ spec/requests/api/v1/github/repositories_spec.rb
@@ -1,34 +1,31 @@
 # frozen_string_literal: true
 
-# require 'swagger_helper'
+require 'swagger_helper'
 
-# RSpec.describe 'api/v1/github/repositories', type: :request do
-#   fixtures :users, :teams, :apps
+RSpec.describe 'api/v1/github/repositories', type: :request do
+  fixtures :users, :teams, :apps
 
-#   before(:each) do
-#     @user = users(:john)
-#     setup_omniauth_mock(@user)
-#     VCR.insert_cassette('rswag/repositories')
-#   end
+  before(:each) do
+    @user = users(:john)
+    setup_omniauth_mock(@user)
+    VCR.insert_cassette('rswag/repositories')
+  end
 
-#   after(:each) do
-#     VCR.eject_cassette
-#   end
+  after(:each) do
+    VCR.eject_cassette
+  end
 
-#   path '/v1/teams/{team_id}/apps/{app_id}/repositories/{id}' do
-#     parameter name: 'team_id', in: :path, type: :int, description: 'team_id'
-#     parameter name: 'app_id', in: :path, type: :int, description: 'app_id'
-#     parameter name: 'id', in: :path, type: :string, description: 'repository_id'
+  path '/v1/teams/{team_id}/apps/{app_id}/repository' do
+    parameter name: 'team_id', in: :path, type: :int, description: 'team_id'
+    parameter name: 'app_id', in: :path, type: :int, description: 'app_id'
 
-#     let(:team_id) { teams(:three).id }
-#     let(:app_id) { apps(:four).id }
-#     let(:owner) { 'department-of-veterans-affairs' }
-#     let(:id) { 'platform-console-api' }
+    let(:team_id) { teams(:three).id }
+    let(:app_id) { apps(:four).id }
 
-#     get 'shows repository' do
-#       response(200, 'OK') do
-#         include_context 'run request test'
-#       end
-#     end
-#   end
-# end
+    get 'shows repository' do
+      response(200, 'OK') do
+        include_context 'run request test'
+      end
+    end
+  end
+end
