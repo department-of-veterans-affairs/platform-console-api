6246993ded055eea80c8b0e851137beddec43424
Holden Hinkle <holden@holdenhinkle.com>
2022-10-06 11:15:43 -0400

    clean up

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -12,11 +12,3 @@ Datadog.configure do |c|
   # Instrumentation
   c.tracing.instrument :rails
 end
-
-# Datadog.configure do |c|
-#   c.use :rails
-#   c.service = 'platform-console-api'
-#   c.tracer.enabled = Rails.env.production?
-#   c.tracer hostname: 'datadog-agent',
-#            port: 8126
-# end
